286b162ab898bd2a87f8a2e99c2ee5fd89891fa9
usami-0322 <usaco0322@gmail.com>
2020-03-13 14:19:22 +0000

    update user修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  get 'home/top'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { 
+      :sessions      => "users/sessions",
+      :registrations => "users/registrations",
+      :passwords     => "users/passwords",
+      omniauth_callbacks: 'users/omniauth_callbacks'
+      }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  root 'home#top'
+  get '/about', to: 'home#about'
+  get '/policy', to: 'home#policy'
+  resources :users, :only => [:index, :show, :update]
+ 
 end
