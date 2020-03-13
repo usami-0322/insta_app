28f645a47ddce9a2c0d72d2bb8cf56fd77b6d96f
usami-0322 <usaco0322@gmail.com>
2020-03-13 14:21:04 +0000

    update devise修正

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -14,5 +13,9 @@ module InstaApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
