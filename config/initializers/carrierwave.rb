f070937f5f94b17d8c6e84edd3f366a2d4248b3f
usami-0322 <usaco0322@gmail.com>
2020-03-24 13:51:50 +0000

    update CarrierWave設定

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+ 
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'proj-image-store'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AKIAWH4TCL6IVTTGQPVI'],
+      aws_secret_access_key: ENV['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'],
+      region: ENV['AWS_REGION'],
+      path_style: true
+    }
+  end
+end
+ 
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
